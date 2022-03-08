66e9ac361086297094f6e46d6f3041729e123529
dawabar <elmore.matthew@gmail.com>
2022-03-08 05:53:55 -0800

    Updates for Pre-Work

---
+++ Labs/Objecive2Lab3.java
@@ -0,0 +1,10 @@
+public class Objecive2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3 average;
+    num1 = 54.3;
+    num2 = 98.7;
+    num3 = 13.2;
+    average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+  }
+}
